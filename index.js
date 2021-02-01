6a9e4ccc795793e38b88cf236d3924efd06580cd
BlopGuy <gvieira.assuntos@outlook.pt>
2021-02-01 14:30:10 +0000

    lab finished + bonuses

--- index.js
+++ index.js
@@ -1,17 +1,60 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length++;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (this.items[pos] === undefined) {
+      throw new Error('OutOfBounds');
+    } else {
+      return this.items[pos];
+    }
+  }
 
-  max() {}
+  max() {
+    let max = this.items.reduce(function (a, b) {
+      return Math.max(a, b);
+    });
 
-  min() {}
+    return max;
+  }
 
-  sum() {}
+  min() {
+    let min = this.items.reduce(function (a, b) {
+      return Math.min(a, b);
+    });
 
-  avg() {}
+    return min;
+  }
+
+  sum() {
+    if(this.length === 0) {
+      return 0;
+    }
+    let sum = this.items.reduce((a, b) => {
+      return a + b;
+    });
+
+    return sum;
+  }
+
+  avg() { 
+    if(this.length === 0) {
+      throw new Error('EmptySortedList');
+    }
+
+    let sum = this.sum();
+    let avg = sum/this.length;
+
+    return avg;
+  }
 }
 
 module.exports = SortedList;
